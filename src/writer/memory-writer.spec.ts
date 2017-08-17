1eefc29ae4601bf545cbdaaf5b444e6f6f657483
Mark Wecke <wecke@neoskop.de>
2017-08-17 11:12:18 +0200

    refactor(writer): memory-writer uses blob-builder

--- src/writer/memory-writer.spec.ts
+++ src/writer/memory-writer.spec.ts
@@ -1,12 +1,16 @@
 import 'mocha';
 import { expect } from 'chai';
+import * as sinon from 'sinon';
 import { UGLMemoryWriter } from './memory-writer';
 
 describe('writer/memory-writer', () => {
     describe('UGLMemoryWriter', () => {
-        let writer : UGLMemoryWriter;
+        let writer : UGLMemoryWriter,
+            Blob : sinon.SinonSpy;
         beforeEach(() => {
-            writer = new UGLMemoryWriter()
+            writer = new UGLMemoryWriter();
+            
+            (global as any).Blob = Blob = sinon.spy();
         });
         
         it('should write into memory', () => {
@@ -15,6 +19,22 @@ describe('writer/memory-writer', () => {
             const expected = "Foobar    00420001337020170917\r\n";
             expect(writer.toString()).to.be.eql(expected);
             expect(writer.toBuffer()).to.be.eql(new Buffer(expected));
+            expect(writer.toUint8Array()).to.be.eql(strToUint8Array(expected));
+            
+            writer.toBlob();
+            
+            expect(Blob.callCount).to.be.equal(1);
+            expect(Blob.lastCall.args[0]).to.be.eql([
+                strToUint8Array('Foobar    '),
+                strToUint8Array('0042'),
+                strToUint8Array('00013370'),
+                strToUint8Array('20170917'),
+                strToUint8Array('\r\n')
+            ])
         });
     })
-})
+});
+
+function strToUint8Array(str : string) : Uint8Array {
+    return new Uint8Array(str.split('').map(c => c.charCodeAt(0)));
+}
